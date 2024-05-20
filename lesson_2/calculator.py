4f4d5291ad83175fec772d68cd817705371c66db
Ben Nelson <bhpnelson@gmail.com>
2024-05-20 15:22:44 -0400

    Basically finished Calculator further_explorations from Lesson 2. \n But I couldn't figure out why it printed 'The result is {output}.'

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,17 +1,24 @@
+# Global language setting, modify this for different languages.
+LANGUAGE = 'en'
+
 import json
 # Import json function to read json file
 
 with open('calculator_messages.json', 'r') as file:
     MESSAGES = json.load(file)
-    
-# Function to format our prompts program-wide
-def prompt(message):
-    print(f"==> {message}")
+
+def messages(message, lang='en'):
+    return MESSAGES[lang][message]
+
+# Function to format our prompts program-wide, including language.
+def prompt(key):
+    message = messages(key, LANGUAGE)
+    print(f'=> {message}')
 
 # Function that validates numbers
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
     return False
@@ -21,55 +28,53 @@ def invalid_number(number_str):
 
 # Say Hello.
 while True:
-    prompt('Welcome to Calculator')
+    prompt('welcome')
 
     # Ask the user for the first number.
-    prompt("What's the first number?")
+    prompt('number_prompt_1')
     number1 = input()
 
     # Validate user input.
     while invalid_number(number1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt('invalid_number')
         number1 = input()
 
     # Ask the user for the second number.
-    prompt("What's the second number?")
+    prompt('number_prompt_2')
     number2 = input()
 
     # Validate user input.
     while invalid_number(number2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt('invalid_number')
         number1 = input()
 
     print(f'{number1} {number2}')
 
     # Ask the user for an operation to perform.
-    prompt("""
-        'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
-        """)
+    prompt('operation_prompt')
     operation = input()
 
     # Validate user input
     while operation not in ["1", "2", "3", "4"]:
-        prompt("You must choose 1, 2, 3, or 4")
+        prompt('invalid_operation')
         operation = input()
 
     # Perform the operation on the two numbers.
     match operation:
         case '1':   # '1' represents addition
-            output = int(number1) + int(number2)
+            output = float(number1) + float(number2)
         case '2':   # '2' represents subtraction
-            output = int(number1) - int(number2)
+            output = float(number1) - float(number2)
         case '3':   # '3' represents multiplication
-            output = int(number1) * int(number2)
+            output = float(number1) * float(number2)
         case '4':   # '4' represents division
-            output = int(number1) / int(number2)
+            output = float(number1) / float(number2)
 
 
     # Print the result to the terminal.
-    print(f"The result is: {output}")
+    print(f'The result is {output}')
 
-    prompt('Would you like to perform another operation? (y/n) ')
+    prompt('another_operation')
     answer = input()
     if answer and answer[0].lower() != 'y':
         break
